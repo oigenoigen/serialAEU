8a4ecc9b1190dcbcf2296e276054074ad13445d9
oigenoigen <32309766+oigenoigen@users.noreply.github.com>
2017-10-16 21:56:09 +0300

    Add files via upload
    too much black magic in this simple file

---
+++ windows_typing.py
@@ -0,0 +1,11 @@
+import win32api
+import win32con
+import time
+args = "34546546.345.345"
+for i in args:
+    if i==".":
+        key=190#i hate fucking windows
+    else:
+        key=ord(i)
+    win32api.keybd_event(key, 0, 0, 0)
+    win32api.keybd_event(key, 0, win32con.KEYEVENTF_KEYUP, 0)
